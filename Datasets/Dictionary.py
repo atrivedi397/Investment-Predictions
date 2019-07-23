2b39cb2758d6b4298a884bf1353e7cee9530c8ec
ANCHAL AITHANI <anchalaithani@gmail.com>
2019-07-23 15:24:19 +0530

    unnecessary columns dropped from datasets

---
+++ Datasets/Dictionary.py
@@ -0,0 +1,7 @@
+answers = {1: [{0: ["20-29", "30-39", "40-49", "50-59", "60-69", "70-79", "Prefer not to say"],
+                1: ["18-19", "20-29", "30-39"]
+                }],
+           2: [{0: ["Intermediate", "High School", "Primary", "No formal education"],
+                1: ["Ph.D/Post Doctorate", "Post Graduation", "Graduation"]
+                }],
+           }
