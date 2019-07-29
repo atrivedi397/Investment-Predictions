c2ff909bd9508e0639ea8bc08cf5c072672350a3
Abhishek <atrivedi397@gmail.com>
2019-07-30 00:11:38 +0530

    good and bad answers counted

--- Files for implementation/manipulating dataset.py
+++ Files for implementation/manipulating dataset.py
@@ -9,6 +9,19 @@ col_to_remove = ["Timestamp", "Please state your gender", "Does your household h
 df = pd.read_csv("/home/atrivedi/Investment-Predictions/Datasets/Investment_Prediction(csv).csv")
 # print(df.shape)
 
+"""Variable dataset declared for the counting of zeroes and ones in the answers"""
+
+"""--- structure = {person1 : {count_zeroes : number1, count_ones : number2}, ......}"""
+count_of_answers_given_by_person = {}
+
+for i in range(0, 215):
+    count_of_answers_given_by_person[i] = {"zeroes_count": 0, "ones_count": 0}
+
+print(count_of_answers_given_by_person)
+
+the_ultimate_response_list = []
+""""""
+
 df.fillna(0, inplace=True)
 columns = list(df)
 
@@ -46,7 +59,8 @@ for questions in range(25):
             response_list[index] = 1
 
     # some answers are having 1.0 (float as their values) .. why?
-    print(f"length {len(response_list)} : ques {questions} : ", response_list)
+    # print(f"length {len(response_list)} : ques {questions} : ", response_list)
+    the_ultimate_response_list.append(response_list)
 
     """for column in range(len(columns)):
         print(columns[column])
@@ -56,6 +70,26 @@ for questions in range(25):
     response_list = []
     ans_list = []
 
+
+"""---------------------------------------------------------------------------------------------------------"""
+for i in range(25):
+    print(the_ultimate_response_list[i])
+
+
+for i in range(214):
+    for container in the_ultimate_response_list:
+        if container[i] == 0:
+            count_of_answers_given_by_person[i]["zeroes_count"] += 1
+        else:
+            count_of_answers_given_by_person[i]["ones_count"] += 1
+
+num = 1
+for dictionary in count_of_answers_given_by_person.values():
+    print(f"person {num}: ", dictionary,
+          f" total sum of zero and one = {dictionary['zeroes_count']+dictionary['ones_count']}")
+    num += 1
+
+print(len(count_of_answers_given_by_person))
 
 """to find how many value are nan/NAN"""
 # print(df[df.isna().any(axis=1)])
