f951dcd0a281e119d6be7a0c43c77f0185b885db
Abhishek Trivedi <atrivedi397@gmail.com>
2019-09-10 21:07:05 +0530

    fin, literacy, behaviour analyzed

--- functions/main.py
+++ functions/main.py
@@ -11,14 +11,14 @@ if __name__ == "__main__":
 
     predicted_outcome = adding_target_to_munged_csv_file(munged_df, "/home/atrivedi/Investment-Predictions/Datasets/Investment_Prediction_classified_data1.csv", count_of_answers_by_person)
 
-    new_csv_weightage_file1 = csv_file_clean("/home/atrivedi/Investment-Predictions/Datasets/Investment_Prediction_classified_data1.csv", columns_removed_from_classified, "/home/atrivedi/Investment-Predictions/Datasets/Investment_Prediction_classified_data__1.csv")
-    new_csv_weightage_file2 = csv_file_clean(
-        "/home/atrivedi/Investment-Predictions/Datasets/Investment_Prediction_classified_data1.csv",
-        columns_removed_from_classified2,
-        "/home/atrivedi/Investment-Predictions/Datasets/Investment_Prediction_classified_data__2.csv")
+    new_csv_weightage_file1 = csv_file_clean("/home/atrivedi/Investment-Predictions/Datasets/Investment_Prediction_classified_data1.csv", columns_removed_for_literacy, "/home/atrivedi/Investment-Predictions/Datasets/Investment_Prediction_without_literacy.csv")
+    new_csv_weightage_file2 = csv_file_clean("/home/atrivedi/Investment-Predictions/Datasets/Investment_Prediction_classified_data1.csv", columns_removed_for_behaviour, "/home/atrivedi/Investment-Predictions/Datasets/Investment_Prediction_without_behaviour.csv")
+    new_csv_weightage_file3 = csv_file_clean("/home/atrivedi/Investment-Predictions/Datasets/Investment_Prediction_classified_data1.csv", columns_removed_for_income, "/home/atrivedi/Investment-Predictions/Datasets/Investment_Prediction_without_income.csv")
 
-    files = ["/home/atrivedi/Investment-Predictions/Datasets/Investment_Prediction_classified_data__1.csv", "/home/atrivedi/Investment-Predictions/Datasets/Investment_Prediction_classified_data__2.csv"]
-    values = ["Report after removing Literacy and Income", "Report after removing Behaviour"]
+    files = ["/home/atrivedi/Investment-Predictions/Datasets/Investment_Prediction_without_income.csv", "/home/atrivedi/Investment-Predictions/Datasets/Investment_Prediction_without_literacy.csv", "/home/atrivedi/Investment-Predictions/Datasets/Investment_Prediction_without_behaviour.csv"]
+
+    values = ["Report after removing Income", "Report after removing Literacy", "Report after removing Behaviour"]
+
     value = 0
     for file in files:
         x_test, y_test, classifier, confusion_matrix, classification_report, accuracy_score = classifier_and_prediction(file)
@@ -28,6 +28,7 @@ if __name__ == "__main__":
         print(f"Classification Report \n {classification_report}")
         print(f"Accuracy Score : {accuracy_score}")
         value += 1
+
 
     """ This will save the model iff the accuracy score is more than 95 % 
         and will tell after how many times of running the model it gets more than 95% accuracy.
--- functions/variables.py
+++ functions/variables.py
@@ -18,14 +18,32 @@ col_to_remove = ["Timestamp", "Please state your gender", "Does your household h
                  "What is your view about saving money?"]
 
 
-columns_removed_from_classified = ["Your highest education level?", "Marital Status",
-                                   "Which bracket does your household income fall into?",
-                                   "Which form of \"more than one\" source of income you have?",
-                                   "Considering all the sources of income, is your household income regular and reliable?",
-                                   "Your retirement is covered under which of the following?"]
+columns_removed_for_literacy = ["Your highest education level?",
+                                #"Who is responsible for day-to-day monetary decisions in your household?",
+                                #"Does your household have a budget?",
+                                "Which of the following statements best describe your pattern for choosing a financial product/service",
+                                "Which source of financial information influences you the most?",
+                                "In the past 12 months, have you been saving money in the following forms?",
+                                "Your retirement is covered under which of the following?",
+                                "Suppose you put ₹10,000 into a <no fee> savings account with a guaranteed interest of 2% per year. You don't make any further payments into this account and you don't withdraw any money. How much would be in the account at the end of the first year, once the interest payment is made?"]
 
-columns_removed_from_classified2 = ["Do you have any such habits?", "Before I buy something, I carefully consider whether I can afford it",
-                                    "I tend to live for today and let tomorrow take care of itself",
-                                    "I find it more satisfying to spend money than to save it for the long term",
-                                    "I pay my bills on time", "I am prepared to risk some of my own money when saving or making an investment",
-                                    "I keep a close personal watch on my financial affairs", "I set a long term financial goals and strive to achieve them"]
+
+columns_removed_for_income = ["Which bracket does your household income fall into?",
+                              "Which form of \"more than one\" source of income you have?",
+                              "Considering all the sources of income, is your household income regular and reliable?"]
+
+
+columns_removed_for_behaviour = ["Before I buy something, I carefully consider whether I can afford it",
+                                 "I tend to live for today and let tomorrow take care of itself",
+                                 "I find it more satisfying to spend money than to save it for the long term",
+                                 "I pay my bills on time",
+                                 "I am prepared to risk some of my own money when saving or making an investment",
+                                 "I keep a close personal watch on my financial affairs",
+                                 "I set a long term financial goals and strive to achieve them",
+                                 "Do you have any such habits?"]
+
+"""
+question_to_remove = "Income includes : 25,26,27" \
+                     "Behaviour includes question number: 13,14,15,16,17,18,19,30" \
+                     "Financial literacy includes : Question number 3,9, 10,11,12,23,28,31" \
+"""                     ""
